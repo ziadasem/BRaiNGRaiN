f70ce83d82b61acd67fa77dfcb927db55af172df
ziad asem <52223041+ziadasem@users.noreply.github.com>
2019-11-09 16:10:51 +0200

    Update cv_example.py

--- cv_example.py
+++ cv_example.py
@@ -1,1 +1,17 @@
 import cv2
+
+#ziad asem 
+#code for webcam and close it by pressing 'q'
+
+capture = cv2.VideoCapture(0);  #open default webcam 
+capture.set(3, 700) #set width of frame
+capture.set(4, 700) #set hieght of frame
+capture.set(5, 20) #set FPS
+capture.set(10, 20) #set brightness
+
+while 1 :
+  1 , img =  capture.read();
+  cv2.imshow("preview", image) ;
+  key = cv2.waitKey(1) & 0xFF        
+  if key == ord("q"):
+    break
